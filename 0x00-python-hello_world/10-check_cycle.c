dcfef915187ec02e07c03964d579f57e287cdacf
kachisapphire <mychaelesther99gmail.com>
2024-01-04 12:02:28 -0800

    cinpython

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,4 +8,16 @@
 int check_cycle(listint_t *list)
 {
 	listint_t *first = list;
-	listint
+	listint_t *second = list;
+
+	if (!list)
+		return (0);
+	while (first && second && second->next)
+	{
+		first = first->next;
+		second = second->next->next;
+		if (first == second)
+			return (1);
+	}
+	return (0);
+}
