1a20853afb8652dd40e4243a73cdecbb3f367e17
kachisapphire <mychaelesther99gmail.com>
2024-01-04 11:46:42 -0800

    hello

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ *check_cycle - function to execute
+ *@listint_t: ....
+ *@list: ..
+ *Return: 0 or 1
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *first = list;
+	listint
