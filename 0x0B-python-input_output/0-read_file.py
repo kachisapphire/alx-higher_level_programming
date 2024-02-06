32775249f946601ea0329c44fd9c4f86e53a8632
kachisapphire <mychaelesther99@gmail.com>
2024-02-06 09:57:08 -0800

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open("my_file_0.txt", encoding="utf-8") as read_file:
-        print(read_file.read())
+    with open("my_file_0.txt", encoding="utf-8") as my_file:
+        file = my_file.read()
+        print(file)
