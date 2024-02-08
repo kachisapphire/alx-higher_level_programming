4860d818d205a921c95c98aaf9522a2ded0aabda
kachisapphire <mychaelesther99@gmail.com>
2024-02-08 05:27:49 -0800

    area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" This module contains a class.
+"""
+
+
+class Square:
+    """ This is a class Square.
+    It is made up of attributes and instances.
+    """
+    def __init__(self, size=0):
+
+        """ This is a documentation for the function.
+        Parameter:
+            size: initialised to zero.
+        """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+
+        """ This function calculates the area of Square.
+        Returns:
+            area of a square.
+        """
+        return self.__size ** 2
