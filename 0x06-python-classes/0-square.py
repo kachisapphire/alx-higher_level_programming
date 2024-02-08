9e7aec6759328423003e6d75707798d1961690a0
kachisapphire <mychaelesther99@gmail.com>
2024-02-08 04:24:31 -0800

    empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+""" This module defines the empty class Square"""
+
+
 class Square:
-    """ this is s simple docsting of a class aquare"""
+
+    """ This is s simple docsting of a class Square.
+    It is currently empty.
+    More functions will be passed into it.
+    """
     pass
