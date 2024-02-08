3a11b7e006882d4eb346eeb8e249cce0040d43b0
kachisapphire <mychaelesther99@gmail.com>
2024-02-08 04:39:11 -0800

    size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" This module defines a class Square.
+"""
+
+
+class Square:
+    """ This is an empty class square.
+    parameters will be added as the code progresses.
+    """
+
+    def __init__(self, size):
+        """ This function takes a private attribute instance size.
+        Parameters:
+        size: no type or value verification.
+        """
+        self.__size = size
