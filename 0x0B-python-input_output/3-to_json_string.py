19f8e8323b151b0def144fea02775f182f3a298a
kachisapphire <mychaelesther99@gmail.com>
2024-02-11 00:41:01 -0800

    json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" This module is about JSON."""
+import json
+
+
+def to_json_string(my_obj):
+    """ This function returns a JSON represntation."""
+    return json.dumps(my_obj)
