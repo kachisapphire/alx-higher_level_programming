ba9bfcf90d39f22ca76cc9f6d1560d8fed626ae8
kachisapphire <mychaelesther99@gmail.com>
2024-02-11 00:01:21 -0800

    module

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,6 +8,6 @@ def read_file(filename=""):
     parameters:
         filename: file to be read
     """
-    with open("my_file_0.txt", encoding="utf-8") as my_file:
+    with open(filename, encoding="utf-8") as my_file:
         file = my_file.read()
-        print(file)
+        print(file, end="")
