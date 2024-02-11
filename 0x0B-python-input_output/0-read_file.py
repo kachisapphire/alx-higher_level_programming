54e487b78a84945e5f20cdb655a1b2be5ebb55c7
kachisapphire <mychaelesther99@gmail.com>
2024-02-10 23:54:11 -0800

    module

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+""" this is a module of a function thats reads a file.
+"""
+
+
 def read_file(filename=""):
+    """ The function read_file reads a file.
+    parameters:
+        filename: file to be read
+    """
     with open("my_file_0.txt", encoding="utf-8") as my_file:
         file = my_file.read()
         print(file)
