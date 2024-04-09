1ad09ee3bf189009c404817b01deb680166ff8b7
kachisapphire <mychaelesther99@gmail.com>
2024-04-09 06:10:43 -0700

    firstname

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" this module contains a class lockedclass. """
 
 
 class LockedClass:
+    """ This is a class with no attribute.
+    slots restricts creation of dynamic attributes.
+    """
     __slots__ = ["first_name"]
