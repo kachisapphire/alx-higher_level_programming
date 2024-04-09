d31180e37a0931888bf1f1cb91ec08d0472dee53
kachisapphire <mychaelesther99@gmail.com>
2024-04-08 23:13:34 -0700

    property

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" This module contains a class rectangle. """
+
+
+class Rectangle:
+    """ This is a class rectangle that defines a rectangle.
+    """
+    def __init__(self, width=0, height=0):
+        """ This function is the instantiation for Rectangle.
+        Parameters: (width, height). """
+        self.width = width
+        self.height = height
+        @property
+        def width(self):
+            return self.__width
+        @width.setter
+        def width(self, value):
+            if not isinstance(width, int):
+                raise TypeError("width must be an integer")
+            if width < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = value
+
+        @property
+        def height(self):
+            return self.__height
+        @height.setter
+        def height(self, value):
+            if not isinstance(height, int):
+                raise TypeError("height must be an integer")
+            if height < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
