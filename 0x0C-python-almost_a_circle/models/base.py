492c54b89d9de2405aad8f51d077e4efdf3cb8b7
kachisapphire <mychaelesther99@gmail.com>
2024-04-22 00:44:34 -0700

    circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ this is a module containing a class base. """
 import json
+import csv
 
 
 class Base:
@@ -58,3 +59,59 @@ class Base:
                 dummy = cls(1, 1)
             dummy.update(**dictionary)
             return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """ this function returns a list of instances. """
+        filename = "{}.json".format(cls.__name__)
+
+        try:
+            with open(filename, "r") as f:
+                dict_list = Base.from_json_string(f.read())
+
+                lists = []
+
+                for i in dict_list:
+                    lists.append(cls.create(**i))
+                return lists
+        except FileNotFoundError:
+            return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """ this function is for serialization. """
+        filename = "{}.csv".format(cls.__name__)
+
+        with open(filename, "w") as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                if cls.__name__ == "Rectangle":
+                    instances = ["id", "width", "height", "x", "y"]
+                else:
+                    instances = ["id", "size", "x", "y"]
+                csvwrite = csv.DictWriter(f, fieldnames=instances)
+
+            for i in list_objs:
+                csvwrite.writerow(i.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ this function is for deserialization. """
+        filename = "{}.csv".format(cls.__name__)
+
+        try:
+            with open(filename, "r") as f:
+                if cls.__name__ == "Rectangle":
+                    instances = ["id", "width", "height", "x", "y"]
+                else:
+                    instances = ["id", "size", "x", "y"]
+                dict_list = csv.DictReader(f, fieldnames=instances)
+                lists = []
+                for i in dict_list:
+                    i = {key: int(value) for key, value in i.items()}
+                    new_list = cls.create(**i)
+                    lists.append(new_list)
+                return lists
+        except FileNotFoundError:
+            return []
