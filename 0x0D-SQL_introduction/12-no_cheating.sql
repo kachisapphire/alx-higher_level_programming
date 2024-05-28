df16177123a3306d6f02b47160c46b99efe7cee2
kachisapphire <mychaelesther99@gmail.com>
2024-05-28 10:23:46 -0700

    task14

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+#!/bin/bash
+-- Write a script that updates the score of Bob to 10 in the table second_table
+-- Update table
+UPDATE second_table SET score = '10' WHERE name = "Bob";
---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,4 @@
+#!/bin/bash
+-- Write a script that removes all records with a score <= 5
+-- Delete table
+DELETE FROM second_table WHERE score <= '5';
