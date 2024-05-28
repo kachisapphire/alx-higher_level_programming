cfe97711fa6bfbf1d919a8ad15f27025b30f9c6a
kachisapphire <mychaelesther99@gmail.com>
2024-05-28 08:47:37 -0700

    task1

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,3 +1,4 @@
 #!/bin/bash
-#Write a script that lists all databases of your MySQL sever
-/* List all database */ SHOW DATABASES;
+-- Write a script that lists all databases of your MySQL sever
+-- List all database
+SHOW DATABASES;
