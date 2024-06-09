06a14ff562dd2963e8dd4ff5a3fe3eddd6953014
kachisapphire <mychaelesther99@gmail.com>
2024-06-09 05:19:58 -0700

    task16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,4 +1,4 @@
 #!/bin/bash
 -- Write a script that lists all shows, and all genres linked to that show
 -- Left join
-SELECT tv_shows.title, tv_genres.name FROM tv_show_genres LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id ORDER BY tv_shows.title ASC, tv_genres.name ASC;
+SELECT tv_shows.title, tv_genres.name FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id ORDER BY tv_shows.title ASC, tv_genres.name ASC;
