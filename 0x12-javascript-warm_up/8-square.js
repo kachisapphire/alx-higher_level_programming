667f0d91d5cb4d0fb90071eb57c2090905d96ac4
kachisapphire <mychaelesther99@gmail.com>
2024-06-20 05:54:32 -0700

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const args = process.argv[2];
+const num = parseInt(args);
+if (isNaN(num)) {
+  console.log('Missing size');
+} else {
+  let i, square, j;
+  for (i = 0; i < num; i++) {
+    square = '';
+    for (j = 0; j < num; j++) {
+      square += 'x';
+    } console.log(square);
+  }
+}
