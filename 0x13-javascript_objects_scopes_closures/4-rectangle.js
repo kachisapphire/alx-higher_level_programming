9a0a287e9331369705710cf1e6fb2d7c24c142a3
kachisapphire <mychaelesther99@gmail.com>
2024-06-20 10:59:10 -0700

    rectangle4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i, j, rect;
+    for (i = 0; i < this.height; i++) {
+      rect = '';
+      for (j = 0; j < this.width; j++) {
+        rect += 'X';
+      } console.log(rect);
+    }
+  }
+
+  rotate () {
+    const tmp = this.height;
+    this.height = this.width;
+    this.width = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
